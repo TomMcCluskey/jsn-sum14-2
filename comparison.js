dc9ac85c202a53b5b85ecdf39598f25599587e33
Tom McCluskey <bosun.tom@gmail.com>
2014-05-23 16:54:44 -0700

    Object copy working

--- comparison.js
+++ comparison.js
@@ -1,5 +1,9 @@
-function Copy(obj) {
-    
+function copy(obj) {
+    objCopy = {}; //is this the best way to have a function make a global variable?
+    for (property in obj) {
+        objCopy[property] = obj[property];
+    }
+    return objCopy;
 }
 
 function equal(objA, ObjB) {
@@ -21,3 +25,6 @@ function intersect(objA, objB) {
 function subtract(objA, objB) {
     
 }
+
+
+var niagara = { name: "Niagara", hull: 'wood', rig: "brig", engines: 2, sails: { staysails: ['Fore Topmast Staysail', 'Outer Jib', 'Flying Jib', 'Main Staysail', 'Main Topmast Staysail', 'Spanker'], squares: ["Fores'l", "Fore Tops'l", "Fore T'Gallant", "Fore Royal", "Mains'l", "Main Tops'l", "Main T'Gallant", "Main Royal"] }, crew: 18 };
