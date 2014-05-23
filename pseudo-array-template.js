a19c826b62c6cb534e63a8def577f8ff23e335cf
Tom McCluskey <bosun.tom@gmail.com>
2014-05-23 15:33:22 -0700

    Finished pseudo-array

--- pseudo-array-template.js
+++ pseudo-array-template.js
@@ -1,13 +1,25 @@
-var array = {length:0};
+var array = {length: 0};
 
-array.pop = function() {
-	//...
-}
+array.pop = function () {
+	var lastValue = this[(this.length) - 1];
+    this[(this.length) - 1] = undefined;
+    this.length -= 1;
+    return lastValue;
+};
 
-array.push = function() {
-	//...
-}
+array.push = function (value) {
+	this[this.length] = value;
+    this.length += 1;
+};
 
-array.join = function() {
-	//...
-}
+array.join = function (char) {
+    var string = this[0];
+	for (var counter = 1; counter < this.length; counter ++) {
+        string += (char || ' ') + this[counter];
+    }
+    return string;
+};
+
+// Question b): That sequence will result in the string 'cab'
+// Question c): I think that will result in the string 'undefined a'.
+// EDIT: Dang, looks like I had the order reversed.
