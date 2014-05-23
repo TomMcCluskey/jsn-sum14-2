fd78c2d77a762b909521dc50c12244d283b6377a
Tom McCluskey <bosun.tom@gmail.com>
2014-05-23 15:36:34 -0700

    added comparison.js

---
+++ comparison.js
@@ -0,0 +1,23 @@
+function Copy(obj) {
+    
+}
+
+function equal(objA, ObjB) {
+    
+}
+
+function similar(objA, objB) {
+    
+}
+
+function union(objA, objB) {
+    
+}
+
+function intersect(objA, objB) {
+    
+}
+
+function subtract(objA, objB) {
+    
+}
