1e0417ce35191dbb0275506254a3e8fe3e45c7d9
Tom McCluskey <bosun.tom@gmail.com>
2014-05-23 19:36:34 -0700

    finished equal function

--- comparison.js
+++ comparison.js
@@ -6,8 +6,23 @@ function copy(obj) {
     return objCopy;
 }
 
-function equal(objA, ObjB) {
-    
+function equal(objA, objB) {
+    function match(primary, secondary) {
+        for (property in primary) {
+            if (secondary[property] != primary[property]) {
+//                console.log(secondary[property] + ' != ' + primary[property]);
+                return false;
+            } else {
+//                console.log(secondary[property] + ' == ' + primary[property]);
+            }
+        }
+        return true;
+    }
+    if (match(objA, objB) && match(objB, objA)) {
+        return true;
+    } else { 
+        return false;
+    }
 }
 
 function similar(objA, objB) {
@@ -27,4 +42,9 @@ function subtract(objA, objB) {
 }
 
 
-var niagara = { name: "Niagara", hull: 'wood', rig: "brig", engines: 2, sails: { staysails: ['Fore Topmast Staysail', 'Outer Jib', 'Flying Jib', 'Main Staysail', 'Main Topmast Staysail', 'Spanker'], squares: ["Fores'l", "Fore Tops'l", "Fore T'Gallant", "Fore Royal", "Mains'l", "Main Tops'l", "Main T'Gallant", "Main Royal"] }, crew: 18 };
+//var niagara = { name: "Niagara", hull: 'wood', rig: "brig", engines: 2, sails: { staysails: ['Fore Topmast Staysail', 'Outer Jib', 'Flying Jib', 'Main Staysail', 'Main Topmast Staysail', 'Spanker'], squares: ["Fores'l", "Fore Tops'l", "Fore T'Gallant", "Fore Royal", "Mains'l", "Main Tops'l", "Main T'Gallant", "Main Royal"] }, crew: 18 };
+
+//var lawrence = copy(niagara);
+//console.log(equal(lawrence, niagara));
+//lawrence.rig = 'snow';
+//console.log(equal(lawrence, niagara));
