0e7e1994aab698a19d9f4e2f5a1f3efe37815808
Tom McCluskey <bosun.tom@gmail.com>
2014-05-26 17:22:33 -0700

    haveMet function working

--- social-network-template.js
+++ social-network-template.js
@@ -1,20 +1,16 @@
 
 var people = {};
 
-function Person(name, friends) {
-    this.name = name;
-    this.friends = friends;
-}
-
 people.index = {};
+
+function verify(name) {
+    if (!people.index[name]) {
+        people.index[name] = { name: name, friends: {} };
+    }
+}
 
 people.meet = function(nameA,nameB) {
 	// check for name property in index, add where appropriate. Increment numbers.
-    function verify(name) {
-        if (!people.index[name]) {
-            people.index[name] = { name: name, friends: {} };
-        }
-    }
     
     function relate(person1, person2) {
         if (!people.index[person1]['friends'][person2]) {
@@ -22,19 +18,29 @@ people.meet = function(nameA,nameB) {
         }
     }
     
-    verify(nameA);
-    verify(nameB);
-    
-    relate(nameA, nameB);
-    relate(nameB, nameA);
-    
-    people.index[nameA]['friends'][nameB] += 1;
-    people.index[nameB]['friends'][nameA] += 1;
+    if (nameA != nameB) {
+        verify(nameA);
+        verify(nameB);
+
+        relate(nameA, nameB);
+        relate(nameB, nameA);
+
+        people.index[nameA]['friends'][nameB] += 1;
+        people.index[nameB]['friends'][nameA] += 1;
+    }
     
 }
 
 people.haveMet = function(nameA,nameB) {
-	//...
+    verify(nameA); // possible downside is that people not in index
+    verify(nameB); // get added to it here
+	if (people.index[nameA]['friends'][nameB]) {
+        return people.index[nameA].friends[nameB];
+    } else {
+        console.log(people.index[nameA]);
+        console.log('nope!');
+        return 0;
+    }
 }
 
 people.friendsOf = function(name) {
@@ -45,13 +51,3 @@ people.friendsOf = function(name) {
 function assert(claim,message) {
     if (!claim) {console.error(message);} else {console.log('all good!');}
 }
-
-/*
-The first thing we need to do is set up a relationship, then we need to 
-increment that relationship. to set up the relationship, the first thing
-we need to do is check to see if both people exist. Then we need to add
-entries for the other people. Check to see if nameA exists. If not, create
-an entry for them. If so, don't create an entry for them. Then see if 
-nameB exists. If not, create an entry. If so, don't. Then add friends
-entries for each to the other, or increment them if they already exist.
-*/
